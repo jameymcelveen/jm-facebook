f7f922a1add6f13b6dcd16f098e81db5fa47fe53
Jamey McElveen <jamey.mcelveen@acstechnologies.com>
2015-06-07 03:54:22 -0400

    firt commit

---
+++ jm-facebook.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: JM Facebook Tools
+*/
+//[jm-facebook-photos]
+function jm_facebook_photos_func( $atts ){
+    return "Facebook Photos";
+}
+add_shortcode( 'jm-facebook-photos', 'jm+facebook_photos_func' );
